984305b85239bc79d7be1a7066e74689136c1429
z <149972831+populated@users.noreply.github.com>
2024-08-01 16:16:08 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from hades.hades import Hades
 import json
 
 config = json.load(open("config.json", "r"))
+update = json.load(open("update.json", "r"))
 
-Updater(current_version=2.9).run()
+Updater(current_version=update["version"]).run()
 Hades().run(token=config["token"])
